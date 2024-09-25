eab32cd4d9e94818749160a750caad9f61cdf2d5
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2024-09-25 23:03:43 +0900

    update structure

--- core/AssetManager.cs
+++ core/AssetManager.cs
@@ -1,17 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace cfEngine.Asset
 {
     public abstract class AssetManager
     {
+        private Dictionary<string, Task> _assetLoadingTasks = new();
         private Dictionary<string, WeakReference<object>> _assetMap = new();
-        protected IReadOnlyDictionary<string, WeakReference<object>> AssetMap => _assetMap;
 
-        public abstract object Load(string path);
-        public abstract T Load<T>(string path) where T : class;
+        public object Load(string path)
+        {
+            var obj = _Load(path);
+            RegisterAsset(path, obj);
+            return obj;
+        }
 
-        protected void Register(string path, object asset)
+        protected abstract object _Load(string path);
+        
+        public T Load<T>(string path) where T : class
+        {
+            var t = _Load<T>(path);
+            RegisterAsset(path, t);
+            return t;
+        }
+
+        protected abstract T _Load<T>(string path) where T : class;
+        
+        private void RegisterAsset(string path, object asset)
         {
             if (_assetMap.TryGetValue(path, out var wr))
             {
