574fd18d724bd2225f11686d8bf1578062023978
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2024-09-27 21:24:47 +0900

    add async load

--- core/AssetManager.cs
+++ core/AssetManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using cfEngine.Logging;
 
 namespace cfEngine.Asset
 {
@@ -11,8 +13,13 @@ namespace cfEngine.Asset
 
         public object Load(string path)
         {
-            var obj = _Load(path);
-            RegisterAsset(path, obj);
+            if (TryGetAsset(path, out var obj))
+            {
+                return obj;
+            }
+            
+            obj = _Load(path);
+            _assetMap[path] = new WeakReference<object>(obj);
             return obj;
         }
 
@@ -20,27 +27,71 @@ namespace cfEngine.Asset
         
         public T Load<T>(string path) where T : class
         {
-            var t = _Load<T>(path);
-            RegisterAsset(path, t);
+            if (TryGetAsset(path, out var obj) && obj is T t)
+            {
+                return t;
+            }
+            
+            t = _Load<T>(path);
+            _assetMap[path] = new WeakReference<object>(t);
             return t;
         }
 
         protected abstract T _Load<T>(string path) where T : class;
-        
-        private void RegisterAsset(string path, object asset)
+
+        public Task<object> LoadAsync(string path, CancellationToken token)
         {
-            if (_assetMap.TryGetValue(path, out var wr))
+            if (_assetLoadingTasks.TryGetValue(path, out var t))
             {
-                if (wr.TryGetTarget(out _))
+                if (t is not Task<object> cachedObjectTask)
                 {
-                    throw new ArgumentException($"Repeated register of asset with key ({path})");
+                    Log.LogWarning($"Detect async loading different task result type but with same path {path}");
+                } 
+                else if(!cachedObjectTask.IsFaulted && !cachedObjectTask.IsCanceled)
+                {
+                    return cachedObjectTask;
                 }
-            } 
-            
-            _assetMap[path] = new WeakReference<object>(asset);
+            }
+
+            var objectTask = _LoadAsync(path, token).ContinueWith(t =>
+                {
+                    var result = t.Result;
+                    _assetMap[path] = new WeakReference<object>(result);
+                    return result;
+                }, token);
+            _assetLoadingTasks[path] = objectTask;
+            return objectTask;
         }
 
-        protected bool TryGetAsset(string path, out object asset)
+        protected abstract Task<object> _LoadAsync(string path, CancellationToken token);
+        
+        public Task<T> LoadAsync<T>(string path, CancellationToken token)
+        {
+            if (_assetLoadingTasks.TryGetValue(path, out var t))
+            {
+                if (t is not Task<T> cachedObjectTask)
+                {
+                    Log.LogWarning($"Detect async loading different task result type but with same path {path}");
+                } 
+                else if(!cachedObjectTask.IsFaulted && !cachedObjectTask.IsCanceled)
+                {
+                    return cachedObjectTask;
+                }
+            }
+
+            var objectTask = _LoadAsync<T>(path, token).ContinueWith(t =>
+                {
+                    var result = t.Result;
+                    _assetMap[path] = new WeakReference<object>(result);
+                    return result;
+                }, token);
+            _assetLoadingTasks[path] = objectTask;
+            return objectTask;
+        }
+
+        protected abstract Task<T> _LoadAsync<T>(string path, CancellationToken token) where T : class;
+        
+        public bool TryGetAsset(string path, out object asset)
         {
             asset = null;
             return _assetMap.TryGetValue(path, out var wr) && wr.TryGetTarget(out asset);
