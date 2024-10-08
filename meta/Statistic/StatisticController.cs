2582e5e1d34f72d3727e9fe6033f5fc122601f67
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2024-10-08 09:37:02 +0800

    update statistic structure

--- meta/Statistic/StatisticController.cs
+++ meta/Statistic/StatisticController.cs
@@ -4,40 +4,27 @@ using System.Text.RegularExpressions;
 
 namespace cfEngine.Meta.Statistic
 {
+    public class Statistic
+    {
+        private double _value;
+        public double Value => _value;
+        public event Action<double> OnUpdate;
+
+        public void RecordOnce()
+        {
+            _value += 1;
+            OnUpdate?.Invoke(_value);
+        }
+    }
+
     public class StatisticController
     {
-        public class Statistic
-        {
-            private double _value;
-            public double Value => _value;
-            private List<WeakReference<StatisticObjective>> _objectiveReferences = new();
-
-            public void RecordOnce()
-            {
-                _value += 1;
-                for (var i = _objectiveReferences.Count - 1; i >= 0; i--)
-                {
-                    if (!_objectiveReferences[i].TryGetTarget(out var objective))
-                    {
-                        _objectiveReferences.RemoveAt(i);
-                        continue;
-                    }
-
-                    objective.FireUpdate();
-                }
-            }
-
-            public void RegisterObjective(StatisticObjective objective)
-            {
-                _objectiveReferences.Add(new WeakReference<StatisticObjective>(objective));
-            }
-        }
-
-        private List<WeakReference<StatisticObjective>> _objectives = new();
         private Dictionary<string, Statistic> _statisticMap = new();
         public Dictionary<string, Statistic> StatisticMap => _statisticMap;
 
-        public void IncrementStatistic(string statisticKey)
+        public event Action<string> OnNewStatisticRecorded; 
+
+        public void Record(string statisticKey)
         {
             if (_statisticMap.TryGetValue(statisticKey, out var statistic))
             {
@@ -46,38 +33,15 @@ namespace cfEngine.Meta.Statistic
             else
             {
                 statistic = new Statistic();
-                for (var i = _objectives.Count - 1; i >= 0; i--)
-                {
-                    if (!_objectives[i].TryGetTarget(out var objective))
-                    {
-                        _objectives.RemoveAt(i);
-                        continue;
-                    }
-
-                    if (Regex.IsMatch(objective.regex, statisticKey))
-                    {
-                        statistic.RegisterObjective(objective);
-                    }
-                }
-
                 _statisticMap[statisticKey] = statistic;
+
+                OnNewStatisticRecorded?.Invoke(statisticKey);
             }
         }
 
-        public StatisticObjective CreateObjective(string regex)
+        public StatisticObjective CreateObjective(string regex, double start, double target)
         {
-            var objective = new StatisticObjective(regex);
-
-            _objectives.Add(new WeakReference<StatisticObjective>(objective));
-            
-            foreach (var (statisticKey, statistic) in _statisticMap)
-            {
-                if(!Regex.IsMatch(statisticKey, regex)) continue;
-                
-                statistic.RegisterObjective(objective);
-            }
-
-            return objective;
+            return new StatisticObjective(regex, start, target, this);
         }
     }
 }
--- meta/Statistic/StatisticObjective.cs
+++ meta/Statistic/StatisticObjective.cs
@@ -1,21 +1,91 @@
 
 using System;
+using System.Collections.Generic;
 
 namespace cfEngine.Meta.Statistic
 {
-    public class StatisticObjective
+    public class StatisticObjective: IDisposable
     {
-        public readonly string regex;
-        public event Action OnStatisticUpdated;
+        private readonly StatisticController _statisticController;
+        public readonly string Regex;
+        public readonly double Start;
+        public readonly double Target;
+        private double _value;
+        public double Value => _value;
+        
+        public event Action<double> OnUpdated;
 
-        public StatisticObjective(string regex)
+        private HashSet<WeakReference<Statistic>> _statisticsRegistered = new();
+
+        public StatisticObjective(string regex, double start, double target, StatisticController statisticController) :
+            this(regex, start, target)
         {
-            this.regex = regex;
+            _statisticController = statisticController;
+            _statisticController.OnNewStatisticRecorded += OnNewStatisticRecorded;
+        }
+        
+        private StatisticObjective(string regex, double start, double target)
+        {
+            this.Regex = regex;
+            this.Target = target;
+            this.Start = start;
+            this._value = 0;
         }
 
-        internal void FireUpdate()
+        private void OnNewStatisticRecorded(string statisticKey)
         {
-            OnStatisticUpdated?.Invoke();
+            var statistic = _statisticController.StatisticMap[statisticKey];
+            var wr = new WeakReference<Statistic>(statistic);
+            if (_statisticsRegistered.Contains(wr))
+            {
+                throw new ArgumentException("Statistic already in objective.");
+            }
+            
+            _statisticsRegistered.Add(wr);
+            statistic.OnUpdate += OnStatisticUpdate;
+        }
+
+        private void OnStatisticUpdate(double value)
+        {
+            List<WeakReference<Statistic>> pendingRemove = new();
+
+            var totalValue = -Start;
+            foreach (var wr in _statisticsRegistered)
+            {
+                if (!wr.TryGetTarget(out var statistic))
+                {
+                    pendingRemove.Add(wr);
+                    continue;
+                }
+
+                totalValue += statistic.Value;
+            }
+
+            _value = totalValue;
+            OnUpdated?.Invoke(_value);
+            
+            foreach (var wr in pendingRemove)
+            {
+                _statisticsRegistered.Remove(wr);
+            }
+            pendingRemove.Clear();
+        }
+        
+        public void Dispose()
+        {
+            if (_statisticController != null)
+            {
+                _statisticController.OnNewStatisticRecorded -= OnNewStatisticRecorded;
+            }
+            foreach (var wr in _statisticsRegistered)
+            {
+                if (wr.TryGetTarget(out var statistic))
+                {
+                    statistic.OnUpdate -= OnStatisticUpdate;
+                }
+            }
+            
+            _statisticsRegistered.Clear();
         }
     }
 }
