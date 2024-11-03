ee8ee32eafcc41ee7f9eedebbc5913e3804f1134
felixwongong <74048524+felixwongong@users.noreply.github.com>
2024-11-03 23:46:57 +0800

    inventory wip

--- meta/Inventory/InventoryController.cs
+++ meta/Inventory/InventoryController.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json.Nodes;
 using cfEngine.Core;
+using ItemIdStr = System.String;
 
 public class InventoryController: IRuntimeSavable, IDisposable
 {
+    private Dictionary<ItemIdStr, InventoryItem> _itemMap = new();
+
     public void Initialize(IReadOnlyDictionary<string, JsonObject> dataMap)
     {
     }
@@ -13,7 +16,37 @@ public class InventoryController: IRuntimeSavable, IDisposable
     {
     }
 
+    public GetInventoryResult GetInventory(GetInventoryOption option)
+    {
+        var resultItems = new List<InventoryItem>(option.fetchItemIds.Count);
+        
+        var fetchItemIds = option.fetchItemIds;
+        foreach (var itemId in fetchItemIds)
+        {
+            if (_itemMap.TryGetValue(itemId, out var item))
+            {
+                resultItems.Add(item);
+            }
+        }
+
+        return new GetInventoryResult()
+        {
+            inventoryItems = resultItems
+        };
+    }
+
     public void Dispose()
     {
     }
+}
+
+public class GetInventoryResult
+{
+    public IReadOnlyList<InventoryItem> inventoryItems;
+}
+
+public class GetInventoryOption
+{
+    public IReadOnlyList<string> fetchItemIds;
+    public int itemsPerFetch;
 }
