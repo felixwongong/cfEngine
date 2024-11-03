2980117f433b413522757cef54f31b1244e741d8
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2024-11-03 22:57:13 +0800

    add inventory controller

---
+++ meta/Inventory/InventoryController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Nodes;
+using cfEngine.Core;
+
+public class InventoryController: IRuntimeSavable, IDisposable
+{
+    public void Initialize(IReadOnlyDictionary<string, JsonObject> dataMap)
+    {
+    }
+
+    public void Save(Dictionary<string, object> dataMap)
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+}
