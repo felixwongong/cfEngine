f150dc869ac4e29ebd8d6c0cf0b10bdceca47bf2
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2024-11-04 23:52:12 +0800

    inventory controller wip

--- meta/Inventory/InventoryController.cs
+++ meta/Inventory/InventoryController.cs
@@ -2,51 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json.Nodes;
 using cfEngine.Core;
-using ItemIdStr = System.String;
 
-public class InventoryController: IRuntimeSavable, IDisposable
+namespace cfEngine.Meta
 {
-    private Dictionary<ItemIdStr, InventoryItem> _itemMap = new();
-
-    public void Initialize(IReadOnlyDictionary<string, JsonObject> dataMap)
+    public partial class InventoryController : IRuntimeSavable, IDisposable
     {
-    }
+        private Dictionary<string, (Guid, InventoryItem)> _itemMap = new();
 
-    public void Save(Dictionary<string, object> dataMap)
-    {
-    }
-
-    public GetInventoryResult GetInventory(GetInventoryOption option)
-    {
-        var resultItems = new List<InventoryItem>(option.fetchItemIds.Count);
-        
-        var fetchItemIds = option.fetchItemIds;
-        foreach (var itemId in fetchItemIds)
+        public void Initialize(IReadOnlyDictionary<string, JsonObject> dataMap)
         {
-            if (_itemMap.TryGetValue(itemId, out var item))
-            {
-                resultItems.Add(item);
-            }
         }
 
-        return new GetInventoryResult()
+        public void AddItem(string itemId, int count)
         {
-            inventoryItems = resultItems
-        };
+            
+        }
+
+        public void Save(Dictionary<string, object> dataMap)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
     }
-
-    public void Dispose()
-    {
-    }
-}
-
-public class GetInventoryResult
-{
-    public IReadOnlyList<InventoryItem> inventoryItems;
-}
-
-public class GetInventoryOption
-{
-    public IReadOnlyList<string> fetchItemIds;
-    public int itemsPerFetch;
 }
--- meta/Inventory/InventoryItem.cs
+++ meta/Inventory/InventoryItem.cs
@@ -1,4 +1,22 @@
-﻿public class InventoryItem
+﻿using System;
+using cfEngine.Meta;
+
+[Serializable]
+public class InventoryItem
 {
+    public readonly string Id;
+    public readonly InventoryConfigInfo Config;
+
+    private int _count;
     
+    public InventoryItem(string id)
+    {
+        Id = id;
+        Config = Game.Info.Get<InventoryConfigInfoManager>().GetOrDefault(Id);
+    }
+
+    public void Gain(int count)
+    {
+        _count += count;
+    }
 }
