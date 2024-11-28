8d0149c0c3984cf54d514b5e0088c6164605754d
felixwongong <74048524+felixwongong@users.noreply.github.com>
2024-11-29 00:27:25 +0800

    add Rx class for stateful variable

---
+++ rx/Rx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace cfEngine.Rt
+{
+    public class Rx<T>: IDisposable
+    {
+        public T Value { get; private set; }
+
+        private CollectionEvents<T> CollectionEvents = new();
+        public ICollectionEvents<T> Events => CollectionEvents;
+
+        public Rx()
+        {
+        }
+        
+        public Rx(T defaultValue)
+        {
+            Value = defaultValue;
+        }
+        
+        public void Set(T value)
+        {
+            var oldValue = Value;
+            Value = value;
+            CollectionEvents.OnUpdateRelay.Dispatch(oldValue, Value);
+        }
+
+        public void SetNoTrigger(T value)
+        {
+            Value = value;
+        }
+
+        public void Dispose()
+        {
+            Value = default;
+            CollectionEvents.Dispose();
+            CollectionEvents = null;
+        }
+        
+        public static implicit operator T(Rx<T> rx) => rx.Value;
+    }
+}
