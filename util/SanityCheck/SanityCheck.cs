25053bbde5c16b3d7466cbacbc46644ad7548f6d
felixwongong <74048524+felixwongong@users.noreply.github.com>
2025-03-16 01:12:12 +0800

    add sanity check exception

---
+++ util/SanityCheck/SanityCheck.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+using cfEngine.Logging;
+
+namespace cfEngine.Util
+{
+    public static class SanityCheck
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool WhenNull<T>(T target, string message = "") where T: class
+        {
+            if (target == null)
+            {
+                Log.LogException(string.IsNullOrEmpty(message)
+                    ? new SanityCheckException()
+                    : new SanityCheckException(message));
+                return true;
+            }
+
+            return false;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool WhenTrue(bool condition, string message = "")
+        {
+            if (condition)
+            {
+                Log.LogException(string.IsNullOrEmpty(message)
+                    ? new SanityCheckException()
+                    : new SanityCheckException(message));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ util/SanityCheck/SanityCheckExceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cfEngine.Util
+{
+    public class SanityCheckException : Exception
+    {
+        public SanityCheckException() : base("A sanity check failed, indicating an unexpected internal state.")
+        {
+        }
+
+        public SanityCheckException(string message) : base(message)
+        {
+        }
+
+        public SanityCheckException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
