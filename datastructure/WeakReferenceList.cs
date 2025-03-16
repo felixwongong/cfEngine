82957bc2c24405f9f079d793dd3f1fb28ce78624
felixwongong <74048524+felixwongong@users.noreply.github.com>
2025-03-17 00:06:26 +0800

    add weak reference list

---
+++ datastructure/WeakReferenceList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using cfEngine.Pooling;
+
+namespace cfEngine.DataStructure
+{
+    public class WeakReferenceListPool<T> : ObjectPool<WeakReferenceList<T>> where T : class
+    {
+        private static WeakReferenceList<T> createMethod()
+        {
+            return new WeakReferenceList<T>();
+        }
+        
+        private static void releaseAction(WeakReferenceList<T> list)
+        {
+            list.Clear();
+        }
+        
+        public WeakReferenceListPool() : base(createMethod, releaseAction)
+        {
+        }
+    }
+    
+    public class WeakReferenceList<T> : List<WeakReference<T>> where T : class
+    {
+        private static WeakReferenceListPool<T> _pool;
+        private static WeakReferenceListPool<T> GetPool => _pool ??= new WeakReferenceListPool<T>();
+        
+        public static WeakReferenceList<T> Create()
+        {
+            return GetPool.Get();
+        }
+        
+        public void Add(T item)
+        {
+            Add(new WeakReference<T>(item));
+        }
+
+        public void Remove(T item)
+        {
+            RemoveAll(weakRef =>
+            {
+                if (weakRef.TryGetTarget(out var target))
+                {
+                    return target == item;
+                }
+
+                return true;
+            });
+
+            TrimExcess();
+            if (Count == 0)
+            {
+                GetPool.Release(this);
+            }
+        }
+    }
+}
---
+++ datastructure/test/WeakReferenceList.Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace cfEngine.DataStructure.test
+{
+    [TestFixture]
+    public class TestWeakReferenceList
+    {
+        public class TestObject
+        {
+        }
+        
+        [Test]
+        public void Test_Create()
+        {
+            var list = WeakReferenceList<TestObject>.Create();
+            Assert.IsNotNull(list);
+        }
+        
+        [Test]
+        public void Test_Add()
+        {
+            var list = WeakReferenceList<TestObject>.Create();
+            var obj = new TestObject();
+            list.Add(obj);
+            Assert.AreEqual(1, list.Count);
+            Assert.IsTrue(list.First().TryGetTarget(out var target));
+            Assert.AreEqual(obj, target);
+        }
+    }
+}
