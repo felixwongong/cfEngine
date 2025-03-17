a5e9d3b2e3b75af91b2f89a493abcdfece4e82a0
felixwongong <74048524+felixwongong@users.noreply.github.com>
2025-03-17 23:20:33 +0800

    add memorydictionary

---
+++ datastructure/MemoryDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cfEngine.DataStructure
+{
+    public class ReadOnlyMemoryCharComparer<T> : IEqualityComparer<ReadOnlyMemory<T>>
+    {
+        private static ReadOnlyMemoryCharComparer<T> _instance;
+        public static ReadOnlyMemoryCharComparer<T> Instance => _instance ??= new ReadOnlyMemoryCharComparer<T>();
+
+        public bool Equals(ReadOnlyMemory<T> x, ReadOnlyMemory<T> y) => x.Span == y.Span;
+
+        public int GetHashCode(ReadOnlyMemory<T> obj) => obj.GetHashCode();
+    }
+
+    
+    public class MemoryDictionary<TMemoryKey, TValue> : Dictionary<ReadOnlyMemory<TMemoryKey>, TValue>
+    {
+        //Add object pooling
+        public static MemoryDictionary<TMemoryKey, TValue> Create() => new MemoryDictionary<TMemoryKey, TValue>();
+        
+        public MemoryDictionary() : base(ReadOnlyMemoryCharComparer<TMemoryKey>.Instance)
+        {
+        }
+    }
+}
