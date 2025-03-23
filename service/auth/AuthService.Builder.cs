5318df9fa8c42b48462f4dac73b804101cdbbea2
felixwongong <74048524+felixwongong@users.noreply.github.com>
2025-03-23 12:15:39 +0800

    add authservice builder

---
+++ service/auth/AuthService.Builder.cs
@@ -0,0 +1,30 @@
+﻿using cfEngine.Util;
+
+namespace cfEngine.Service.Auth
+{
+    public partial class AuthService
+    {
+        public class Builder
+        {
+            private IAuthService _authService;
+            
+            public Builder SetService(IAuthService authService)
+            {
+                _authService = authService;
+                return this;
+            }
+            
+            public Builder RegisterPlatform(PlatformAuth platform)
+            {
+                SanityCheck.WhenNull(_authService, $"AuthService is not set, call {nameof(SetService)} first");
+                _authService.RegisterPlatform(platform);
+                return this;
+            }
+
+            public IAuthService Build()
+            {
+                return _authService;
+            }
+        }
+    }
+}
