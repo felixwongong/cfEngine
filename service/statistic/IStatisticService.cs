1ca3063b14c65d18c89a63f7107f213d49639e43
felixwongong <74048524+felixwongong@users.noreply.github.com>
2025-04-23 00:36:42 +0800

    service mvc wip

---
+++ service/statistic/IStatisticService.cs
@@ -0,0 +1,12 @@
+﻿#if CF_STATISTIC
+
+namespace cfEngine.Service.Statistic
+{
+    public interface IStatisticService: IService {
+        public void Record(string statisticKey);
+        public StatisticObjective CreateObjective(string regex, double start, double target = -1);
+        public StatisticObjective CreateForwardObjective(string regex, double target = -1);
+        
+    }
+}
+#endif
