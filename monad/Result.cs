549d6893956ba42a2a1ffddf8b8148ab255285d6
felixwongong <74048524+felixwongong@users.noreply.github.com>
2025-05-07 23:42:40 +0800

    add monad result class

---
+++ monad/Result.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace cfEngine.Monad
+{
+    public struct Result<T, E>
+    {
+        public T value { get; private set; }
+        public E errorValue { get; private set; }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Result<T, E> Ok(T value)
+        {
+            return new Result<T, E> { value = value };
+        }
+
+        public static Result<T, E> Err(E errorValue)
+        {
+            return new Result<T, E> { errorValue = errorValue };
+        }
+    }
+}
