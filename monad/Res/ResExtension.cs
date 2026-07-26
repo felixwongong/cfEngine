ad6852fb2b0dbfee035d2d5eae01a1ce69344713
Felix Wong <felixwong@nec.com>
2026-07-26 23:55:34 +0800

    add res.bind

--- monad/Res/ResExtension.cs
+++ monad/Res/ResExtension.cs
@@ -8,10 +8,52 @@ namespace cfEngine
     {
         public static Res<T, Exception> Ok<T>([DisallowNull] T value) => Res<T, Exception>.Ok(value);
         public static Res<T, Exception> Err<T>([DisallowNull] Exception error) => Res<T, Exception>.Err(error);
+
+        public static Res<(T1, T2), TErr> Combine<T1, T2, TErr>(Res<T1, TErr> a, Res<T2, TErr> b)
+        {
+            if (a.HasError(out var error)) return Res<(T1, T2), TErr>.Err(error);
+            if (b.HasError(out error)) return Res<(T1, T2), TErr>.Err(error);
+            return Res<(T1, T2), TErr>.Ok((a.value, b.value));
+        }
+
+        public static Res<(T1, T2, T3), TErr> Combine<T1, T2, T3, TErr>(
+            Res<T1, TErr> a, Res<T2, TErr> b, Res<T3, TErr> c)
+        {
+            if (a.HasError(out var error)) return Res<(T1, T2, T3), TErr>.Err(error);
+            if (b.HasError(out error)) return Res<(T1, T2, T3), TErr>.Err(error);
+            if (c.HasError(out error)) return Res<(T1, T2, T3), TErr>.Err(error);
+            return Res<(T1, T2, T3), TErr>.Ok((a.value, b.value, c.value));
+        }
+
+        public static Res<(T1, T2, T3, T4), TErr> Combine<T1, T2, T3, T4, TErr>(
+            Res<T1, TErr> a, Res<T2, TErr> b, Res<T3, TErr> c, Res<T4, TErr> d)
+        {
+            if (a.HasError(out var error)) return Res<(T1, T2, T3, T4), TErr>.Err(error);
+            if (b.HasError(out error)) return Res<(T1, T2, T3, T4), TErr>.Err(error);
+            if (c.HasError(out error)) return Res<(T1, T2, T3, T4), TErr>.Err(error);
+            if (d.HasError(out error)) return Res<(T1, T2, T3, T4), TErr>.Err(error);
+            return Res<(T1, T2, T3, T4), TErr>.Ok((a.value, b.value, c.value, d.value));
+        }
+
+        public static Res<(T1, T2, T3, T4, T5), TErr> Combine<T1, T2, T3, T4, T5, TErr>(
+            Res<T1, TErr> a, Res<T2, TErr> b, Res<T3, TErr> c, Res<T4, TErr> d, Res<T5, TErr> e)
+        {
+            if (a.HasError(out var error)) return Res<(T1, T2, T3, T4, T5), TErr>.Err(error);
+            if (b.HasError(out error)) return Res<(T1, T2, T3, T4, T5), TErr>.Err(error);
+            if (c.HasError(out error)) return Res<(T1, T2, T3, T4, T5), TErr>.Err(error);
+            if (d.HasError(out error)) return Res<(T1, T2, T3, T4, T5), TErr>.Err(error);
+            if (e.HasError(out error)) return Res<(T1, T2, T3, T4, T5), TErr>.Err(error);
+            return Res<(T1, T2, T3, T4, T5), TErr>.Ok((a.value, b.value, c.value, d.value, e.value));
+        }
     }
     
     public partial struct Res<TOk, TErr>
     {
+        public Res<T, TErr> Bind<T>(Func<TOk, Res<T, TErr>> binder)
+        {
+            return _isOk ? binder(_value) : Res<T, TErr>.Err(_error);
+        }
+
         public Res<T, TErr> Map<T>(Func<TOk, T> mapper)
         {
             return _isOk ? Res<T, TErr>.Ok(mapper(_value)) : Res<T, TErr>.Err(_error);
